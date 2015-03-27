f33884c5b15d816831db8ef054fe21d450d804ff
mukulkatal <mukul.katal@venturepact.com>
2015-03-27 13:02:35 +0530

    Intial Dhamaka Commit

---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+<!-- button click -->
+angular.module('starter', ['ionic'])
+
+.controller('PlaylistsCtrl', function($scope, $ionicPopup, $timeout) {
+  $scope.data = {}
+
+  // Triggered on a button click, or some other target
+  $scope.showPopup = function() {
+    var alertPopup = $ionicPopup.alert({
+        title: 'Company Name',
+        template: 'Graph'
+      
+    });
+    alertPopup.then(function(res) {
+      console.log('Company Details');
+    });
+  };
+});
+
+
